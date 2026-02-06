2578de7501c8656b7f0464b137adb36dfb76646e
Cendikia Abdi <cendekiaabdi@gmail.com>
2026-02-06 13:40:53 +0900

    tailwind config done

--- src/app/pages/Homepage.tsx
+++ src/app/pages/Homepage.tsx
@@ -1,15 +1,8 @@
 import { APITester } from "../components/APITester";
-import logo from "../../../static/logo.svg";
-import reactLogo from "../../../static/react.svg";
 
 export function HomePage() {
   return (
-    <div className="app">
-      <div className="logo-container">
-        <img src={logo} alt="Bun Logo" className="logo bun-logo" />
-        <img src={reactLogo} alt="React Logo" className="logo react-logo" />
-      </div>
-
+    <div className="app bg-black">
       <h1>Bun + React</h1>
       <p>
         Edit <code>src/App.tsx</code> and save to test HMR
