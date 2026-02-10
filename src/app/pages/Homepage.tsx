43919810eb3d85905748813789a64fdf0a58696a
Cendikia Abdi <cendekiaabdi@gmail.com>
2026-02-10 18:16:16 +0900

    test deploy

--- src/app/pages/Homepage.tsx
+++ src/app/pages/Homepage.tsx
@@ -6,7 +6,7 @@ export function HomePage() {
     <div className="homepage w-screen h-screen">
         <div className="bg-dark-secondary rounded-lg p-8 border border-dark-border text-dark-text flex flex-col gap-2">
             <div className="text-center text-7xl align-middle grow mb-3 font-bold">DNDate</div>
-            <h1 className="w-full text-center">For when you want to dnd but just can't agree on a date</h1>
+            <h1 className="w-full text-center">For when you want to dnd but just can't agree on a date HERE WE GO</h1>
             <h1 className="w-full text-center">Inspired by <a href="https://tonton.amaneku.com/" target="_blank" className="font-medium text-fg-brand underline hover:no-underline">Tonton Amaneku</a></h1>
             <hr className="h-px my-2 bg-white border-0"/>
             <div className="flex flex-row justify-center">
